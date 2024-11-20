56aa01eabdbe6c8b569565b25970d44f7f6192a7
bhuvi97512 <123265714+bhuvi97512@users.noreply.github.com>
2024-11-20 05:39:44 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+/*
+let score =" 33sf"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+*/
+
+// "33" => 33
+// " 33an" => NaN
+// true = 1 ; false = 0
+
+let isLoggedIn = 4545;
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false 
+// "hello"  => true;
+
+let someNumber = 34
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
