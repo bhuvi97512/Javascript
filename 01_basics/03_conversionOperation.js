4d98b8ef2501504f44522f4b9de995c6c68e1940
bhuvi97512 <123265714+bhuvi97512@users.noreply.github.com>
2024-11-21 06:10:26 +0000

    conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,41 @@ let booleanisLoggedIn = Boolean(isLoggedIn);
 let someNumber = 34
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+// ************************************************* Operations ****************************************************************
+
+let value = 3;
+let negValue = -value;
+
+//console.log(negValue);
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3)
+// console.log(2 / 2)
+// console.log(2 % 2)
+
+let str1 = "Bhuneshwar"
+let str2 = " Kumar"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log( 1 + "2");
+// console.log( "1" + 2 + 2);
+// console.log( 1 + 2 + "2");
+
+// console.log(+true)
+// console.log(+"")
+
+let num1 , num2 , num3 
+num1 = num2  = num3 = 2 + 2
+ 
+let gameCounter = 100
+ ++gameCounter
+ console.log(gameCounter)
