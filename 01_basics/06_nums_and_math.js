b7493ed32b115fe0c708980f8a895788bc47ae34
bhuvi97512 <123265714+bhuvi97512@users.noreply.github.com>
2024-11-24 09:13:33 +0000

    added some maths features

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -34,3 +34,8 @@ const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min )                  // very important remember this formula
+
+
+// about maths note
+
+// console.log(Math) - Math
